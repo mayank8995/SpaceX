e13b9a471759fb39e7543da8e9266291b0e52d93
Mayank Gupta <mayankgupta8995@gmail.com>
2021-03-18 14:23:57 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/space-x'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/SpaceX'));
 
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname,'/dist/space-x/index.html'));
-// });
+app.get('/*', function (req, res) {
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+    res.sendFile(path.join(__dirname + '/dist/SpaceX/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
